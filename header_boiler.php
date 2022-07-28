e0c158038fea6c9715570222083598602d5b3c89
Nicolas Mesa <mesa0011@algonquinlive.com>
2022-07-28 18:36:21 -0500

    .

--- header_boiler.php
+++ header_boiler.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Document</title>
 </head>
 <body>
     
-</body>
-</html>
+   <header>
+        <nav>
+            <a href="<?php echo 'signUp.php'; ?>">Create a new account</a>
+            <a href="<?php echo 'login.php'; ?>">Login</a>
+        </nav>
+   </header>
+    
