e4dfe3f0e133048db8207b1c8b7846569c7a3317
Santiago Mesa <mesa0012@algonquinlive.com>
2022-08-02 23:33:26 -0400

    .

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
+
 <?php include ("headboiler.html"); ?>
 <?php include ("headerboiler.html"); ?>
 <body>
     <h2> This web let you search your favorite movies and store it in your profile! </h2>
-    
-=======
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,6 +15,5 @@
    <?php require_once("dbhelper.php") ?>
 
    <h1>Find movies or shows in here</h1>
->>>>>>> e47129b4140667aba30314dda51fac7ab77d613c
 </body>
 </html>
