b456759ccd138739c3c05d52e31831d81071fd14
Santiago Mesa <mesa0012@algonquinlive.com>
2022-08-02 23:38:11 -0400

    avoid to echo html before POST

--- movies.php
+++ movies.php
@@ -1,23 +1,6 @@
 <?php
     session_start();
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $search = $_POST['movie'];
-        $request = "http://www.omdbapi.com/?t=$search&plot=full&type=movie&apikey=9ab90ab5&";
-        $array_movies = file_get_contents($request);
-        $json = json_decode($array_movies, true);
-        /*
-        foreach($json as $i){
-            echo $i;
-        }
-        */
-        print_r ($json);
-        $title = $json['Title'];
-        $poster = $json['Poster'];
-        $year = $json['Year'];
-        $plot = $json['Plot'];
-        $rating = $json['Ratings'][1]['Value'];
 
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,14 +18,34 @@
         <button type="submit">Search</button>
     </form>
     <?php
-    echo "<img src='$poster'>
-    <div>
-    <h3>$title</h3>
-    <h4>$year</h4>
-    <p>$plot</p>
-    <p>$rating</p>
-    <button type='button'> Add to my profile </button>
-    ";
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $search = $_POST['movie'];
+            $request = "http://www.omdbapi.com/?t=$search&plot=full&type=movie&apikey=9ab90ab5&";
+            $array_movies = file_get_contents($request);
+            $json = json_decode($array_movies, true);
+            /*
+            foreach($json as $i){
+                echo $i;
+            }
+            */
+            print_r ($json);
+            $title = $json['Title'];
+            $poster = $json['Poster'];
+            $year = $json['Year'];
+            $plot = $json['Plot'];
+            $rating = $json['Ratings'][1]['Value'];
+            
+            echo "<img src='$poster'>
+            <div>
+            <h3>$title</h3>
+            <h4>$year</h4>
+            <p>$plot</p>
+            <p>$rating</p>
+            <button type='button'> Add to my profile </button>
+            ";
+    
+        }
+  
     ?>
 
    
