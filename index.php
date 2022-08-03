5f419ebade306279de4e74f2e7733d17988d8344
Santiago Mesa <mesa0012@algonquinlive.com>
2022-08-02 23:40:53 -0400

    .

--- index.php
+++ index.php
@@ -3,11 +3,8 @@
 <?php include ("headerboiler.html"); ?>
 <body>
     <h2> This web let you search your favorite movies and store it in your profile! </h2>
-<<<<<<< HEAD
     
 
-=======
->>>>>>> e4dfe3f0e133048db8207b1c8b7846569c7a3317
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,9 +17,6 @@
    <?php require_once("dbhelper.php") ?>
 
    <h1>Find movies or shows in here</h1>
-<<<<<<< HEAD
 
-=======
->>>>>>> e4dfe3f0e133048db8207b1c8b7846569c7a3317
 </body>
 </html>
--- series.php
+++ series.php
@@ -1,18 +1,6 @@
 <?php
     session_start();
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $search = $_POST['movie'];
-        $request = "http://www.omdbapi.com/?t=$search&plot=full&type=series&apikey=9ab90ab5&";
-        $array_movies = file_get_contents($request);
-        $json = json_decode($array_movies, true);
-        print_r ($json);
-        $title = $json['Title'];
-        $poster = $json['Poster'];
-        $year = $json['Year'];
-        $plot = $json['Plot'];
-        $rating = $json['Ratings'][1]['Value'];
-
-    }
+ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,14 +18,27 @@
         <button type="submit">Search</button>
     </form>
     <?php
-    echo "<img src='$poster'>
-    <div>
-    <h3>$title</h3>
-    <h4>$year</h4>
-    <p>$plot</p>
-    <p>$rating</p>
-    <button type='button'> Add to my profile </button>
-    ";
+       if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $search = $_POST['movie'];
+        $request = "http://www.omdbapi.com/?t=$search&plot=full&type=series&apikey=9ab90ab5&";
+        $array_movies = file_get_contents($request);
+        $json = json_decode($array_movies, true);
+        print_r ($json);
+        $title = $json['Title'];
+        $poster = $json['Poster'];
+        $year = $json['Year'];
+        $plot = $json['Plot'];
+        $rating = $json['Ratings'][1]['Value'];
+        echo "<img src='$poster'>
+        <div>
+        <h3>$title</h3>
+        <h4>$year</h4>
+        <p>$plot</p>
+        <p>$rating</p>
+        <button type='button'> Add to my profile </button>
+        ";
+    }
+   
     ?>
 </body>
 </html>
