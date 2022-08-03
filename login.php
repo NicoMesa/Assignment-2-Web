118ea6a314dded5951bd188fd0813c8e18ae25c2
Santiago Mesa <mesa0012@algonquinlive.com>
2022-08-02 23:36:44 -0400

    .

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php include ("headerboiler.html"); ?>
 <body>
     <a class="top" href="index.php">Back to home page</a>
@@ -15,31 +14,10 @@
             <label for="login">User Name</label>
             <input type="text" name="login" id="login" placeholder="User name">
             <span id="loginError" class="error"></span>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="js/scripts.js" defer></script>
-
-    <title>Login</title>
-</head>
-
-<body>
-    <h2>Login with existing username</h2>
-    <form action="profile.html" method="post" id="loginform" >
-        <div class="textfield">
-            <label for="userlogin">User Name</label>
-            <input type="text" name="userlogin" id="userlogin" placeholder="User name">
-            <span id="userLoginError" class="error"></span>
->>>>>>> e47129b4140667aba30314dda51fac7ab77d613c
         </div>
 
         <div class="textfield">
             <label for="pass">Password</label>
-<<<<<<< HEAD
             <input type="password" name="pass" id="pass" placeholder="Password">
             <span id="passError" class="error"></span>
         </div>
@@ -53,12 +31,5 @@
 
     </form>
    
-=======
-            <input type="password" name="userPass" id="userPass" placeholder="Password">
-            <span id="userPassError" class="error"></span>
-        </div>
-        <button type="submit">Login In</button>
-    </form>
->>>>>>> e47129b4140667aba30314dda51fac7ab77d613c
 </body>
 </html>
