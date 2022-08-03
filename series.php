1e91c2d4694dd05ceeaf9ac52eadcc78811c6759
Santiago Mesa <mesa0012@algonquinlive.com>
2022-08-02 23:19:03 -0400

    Add functionality to search movies and series and display title, year, plot and rating.

---
+++ series.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $search = $_POST['movie'];
+        $request = "http://www.omdbapi.com/?t=$search&plot=full&type=series&apikey=9ab90ab5&";
+        $array_movies = file_get_contents($request);
+        $json = json_decode($array_movies, true);
+        print_r ($json);
+        $title = $json['Title'];
+        $poster = $json['Poster'];
+        $year = $json['Year'];
+        $plot = $json['Plot'];
+        $rating = $json['Ratings'][1]['Value'];
+
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <?php include ("header_boiler.html"); ?>
+    <h2>Add here your favorite series and shows</h2>
+    <form method="POST">
+        <input type="text" placeholder="Search.." name="movie" id="search">
+        <button type="submit">Search</button>
+    </form>
+    <?php
+    echo "<img src='$poster'>
+    <div>
+    <h3>$title</h3>
+    <h4>$year</h4>
+    <p>$plot</p>
+    <p>$rating</p>
+    <button type='button'> Add to my profile </button>
+    ";
+    ?>
+</body>
+</html>
