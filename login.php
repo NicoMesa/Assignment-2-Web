106425f1994a460e4dd2c4150c95f821d3ea26ea
Nicolas Mesa <mesa0011@algonquinlive.com>
2022-08-05 21:32:51 -0500

    begin css code

--- login.php
+++ login.php
@@ -4,20 +4,21 @@ include_once("dbhelper.php");
  ?>
  <body>
     <h2>Login with existing username</h2>
-        <form action="profile.php" method="post" id="loginform" >
-            <div class="textfield">
-                <label for="userlogin">User Name</label>
-                <input type="text" name="userlogin" id="userlogin" placeholder="User name">
-                <span id="userLoginError" class="error"></span>
-            </div>
-
-            <div class="textfield">
-                <label for="pass">Password</label>
-                <input type="password" name="userPass" id="userPass" placeholder="Password">
-                <span id="userPassError" class="error"></span>
-            </div>
-            <button type="submit">Login In</button>
-        </form>
+        <div class="login">
+            <form action="profile.php" method="post" id="loginform" >
+                <div class="textfield">
+                    <label for="userlogin">User Name</label>
+                    <input type="text" name="userlogin" id="userlogin" placeholder="User name">
+                    <span id="userLoginError" class="error"></span>
+                </div>
+                <div class="textfield">
+                    <label for="pass">Password</label>
+                    <input type="password" name="userPass" id="userPass" placeholder="Password">
+                    <span id="userPassError" class="error"></span>
+                </div>
+                <button type="submit">Login In</button>
+            </form>
+        </div>
         
 </body>
 </html>
--- register.php
+++ register.php
@@ -1,37 +1,37 @@
 <?php 
 //include html headers
 include ("headboiler.html"); 
-include ("headerboiler.html"); 
+
 ?>
 <body>
-    <h2>Register as a new user</h2>
-    <!-- Form to get values from user, data will be handled in the bakcend after submit -->
-    <form action="new_user.php" method="post" id="register" onsubmit="return newUser()">
-        <div class="textfield">
-            <label for="email">Email Address</label>
-            <input type="text" name="email" id="email" placeholder="Email">
-            <span id="mailError" class="error"></span>
-        </div>
-
-        <div class="textfield">
-            <label for="login">User Name</label>
-            <input type="text" name="login" id="login" placeholder="User name">
-            <span id="loginError" class="error"></span>
-        </div>
-
-        <div class="textfield">
-            <label for="pass">Password</label>
-            <input type="password" name="pass" id="pass" placeholder="Password">
-            <span id="passError" class="error"></span>
-        </div>
-    
-        <div class="textfield">
-            <label for="pass2">Re-type Password</label>
-            <input type="password" id="pass2" placeholder="Password">
-            <span id="pass2Error" class="error"></span>
-        </div>
-        <button type="submit">Create User</button>
-    </form>
+    <div class="newuser">
+        <h2>Register as a new user</h2>
+        <!-- Form to get values from user, data will be handled in the bakcend after submit -->
+        <form action="new_user.php" method="post" id="register" onsubmit="return newUser()">
+            <div class="textfield">
+                <label for="email">Email Address</label>
+                <input type="text" name="email" id="email" placeholder="Email">
+                <span id="mailError" class="error"></span>
+            </div>
+            <div class="textfield">
+                <label for="login">User Name</label>
+                <input type="text" name="login" id="login" placeholder="User name">
+                <span id="loginError" class="error"></span>
+            </div>
+            <div class="textfield">
+                <label for="pass">Password</label>
+                <input type="password" name="pass" id="pass" placeholder="Password">
+                <span id="passError" class="error"></span>
+            </div>
+        
+            <div class="textfield">
+                <label for="pass2">Re-type Password</label>
+                <input type="password" id="pass2" placeholder="Password">
+                <span id="pass2Error" class="error"></span>
+            </div>
+            <button type="submit">Create User</button>
+        </form>
+    </div>
 
 
    
