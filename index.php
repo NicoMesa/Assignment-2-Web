358968c86f80f3d2c229adc09d9cb96cd6493df7
Yuji Sato <rfyujisato@gmail.com>
2022-08-07 04:26:23 -0400

    Home

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 //include html templates
 include ("headboiler.html"); 
 include ("headerboiler.html"); ?>
-<body>
+<body class="home">
     <?php
     //check if there is a session active to greet the user
     if(isset($_SESSION['username'])){
@@ -16,9 +16,9 @@ include ("headerboiler.html"); ?>
    ?>
 <!-- rest of the page to show what the page does -->
     <div class="searchInfo">
-        <h1> This web let you search your favorite movies and store it in your profile! </h1>
-        <h2> Search thousands of movies and series and add them to your watchllist!</h2>
-        <h3> <i>Never forget what movie or show to watch next</i> </h3>
+        <h1> This web let you search your favorite movies and store it in your profile</h1>
+        <h2> Search thousands of movies and series and add them to your watchlist</h2>
+        <h3> Never forget what movie or show to watch next</h3>
     </div>
 </body>
 </html>
